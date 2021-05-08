947835306c69ef66e18741c163462f7dc973147c
idrach55 <isaac@drachman.org>
2021-05-08 17:07:05 -0400

    connect to either localhost or ropsten

--- react-site/src/util/interact.js
+++ react-site/src/util/interact.js
@@ -1,12 +1,32 @@
 import detectEthereumProvider from '@metamask/detect-provider';
+var Web3 = require('web3');
 
+/*
+Below code connects to the blockchain and loads the contract ABI.
+
+Set network in .env.development.local. 
+If "ropsten": connect using Alchemy web3
+If "localhost": connect using web3.js
+*/
 const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
+const network = process.env.REACT_APP_NETWORK;
+const contractABI = require('../MyNFT.json').abi;
 
-const contractABI = require('../MyNFT.json').abi
-const contractAddress = "0x0C03E84aceB83Dbd2F4f75D43AE23ff96851F9d7";
-
-const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
-const web3 = createAlchemyWeb3(alchemyKey); 
+var web3;
+let contractAddress;
+if (network == "ropsten") 
+{
+    console.log("network: ropsten. connecting alchemy web3...")
+    const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+    web3 = createAlchemyWeb3(alchemyKey); 
+    contractAddress = "0x0C03E84aceB83Dbd2F4f75D43AE23ff96851F9d7";
+} 
+else if (network == "localhost") 
+{
+    console.log("network: localhost. connecting web3...")
+    web3 = new Web3('http://localhost:8545');
+    contractAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+}
 
 // New Metamask connection logic.
 export const connectWallet = async () => {
